2ff2cb8940f5c3b55ac38de1fcb4543f5dd783cc
Paul Michalik <paul.michalik@outlook.com>
2018-09-18 19:16:48 +0000

    Factor "Date" into separate module

---
+++ src/Date.h
@@ -0,0 +1,56 @@
+#ifndef FILTER_DATE_H
+#define FILTER_DATE_H
+
+#include <ctime>
+
+namespace algorithm {
+
+    class Date {
+        private:
+        int year_;
+        int month_;
+        int day_;
+
+        public:
+        Date(int year_, int month_, int day_)
+            : year_(year_), month_(month_), day_(day_) {}
+
+        long getTime() {
+            std::tm tm{0, 0, 0, day_, month_, year_ - 1900};
+            return std::mktime(&tm);
+        }
+
+        friend bool operator==(const Date &lhs, const Date &rhs) {
+            return lhs.year_ == rhs.year_ && lhs.month_ == rhs.month_ &&
+                lhs.day_ == rhs.day_;
+        }
+
+        friend bool operator!=(const Date &lhs, const Date &rhs) {
+            return !(rhs == lhs);
+        }
+
+        friend bool operator<(const Date &lhs, const Date &rhs) {
+            if (lhs.year_ < rhs.year_)
+            return true;
+            if (rhs.year_ < lhs.year_)
+            return false;
+            if (lhs.month_ < rhs.month_)
+            return true;
+            if (rhs.month_ < lhs.month_)
+            return false;
+            return lhs.day_ < rhs.day_;
+        }
+
+        friend bool operator>(const Date &lhs, const Date &rhs) { return rhs < lhs; }
+
+        friend bool operator<=(const Date &lhs, const Date &rhs) {
+            return !(rhs < lhs);
+        }
+
+        friend bool operator>=(const Date &lhs, const Date &rhs) {
+            return !(lhs < rhs);
+        }
+    };
+}
+
+#endif // FILTER_DATE_H
--- src/Person.h
+++ src/Person.h
@@ -1,56 +1,10 @@
 #ifndef FILTER_PERSON_H
 #define FILTER_PERSON_H
 
+#include "Date.h"
 #include <string>
 
 namespace algorithm {
-
-class Date {
-private:
-  int year_;
-  int month_;
-  int day_;
-
-public:
-  Date(int year_, int month_, int day_)
-      : year_(year_), month_(month_), day_(day_) {}
-
-  long getTime() {
-    std::tm tm{0, 0, 0, day_, month_, year_ - 1900};
-    return std::mktime(&tm);
-  }
-
-  friend bool operator==(const Date &lhs, const Date &rhs) {
-    return lhs.year_ == rhs.year_ && lhs.month_ == rhs.month_ &&
-           lhs.day_ == rhs.day_;
-  }
-
-  friend bool operator!=(const Date &lhs, const Date &rhs) {
-    return !(rhs == lhs);
-  }
-
-  friend bool operator<(const Date &lhs, const Date &rhs) {
-    if (lhs.year_ < rhs.year_)
-      return true;
-    if (rhs.year_ < lhs.year_)
-      return false;
-    if (lhs.month_ < rhs.month_)
-      return true;
-    if (rhs.month_ < lhs.month_)
-      return false;
-    return lhs.day_ < rhs.day_;
-  }
-
-  friend bool operator>(const Date &lhs, const Date &rhs) { return rhs < lhs; }
-
-  friend bool operator<=(const Date &lhs, const Date &rhs) {
-    return !(rhs < lhs);
-  }
-
-  friend bool operator>=(const Date &lhs, const Date &rhs) {
-    return !(lhs < rhs);
-  }
-};
 
 class Person {
 public:
@@ -75,4 +29,4 @@ public:
   void setBirthDate(Date birthDate) { birthDate_ = birthDate; }
 };
 }
-#endif // FILTER_Person_H
+#endif // FILTER_PERSON_H
