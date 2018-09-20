36cb0a5f517964678ea34e11edfa283cdc8355d5
Paul Michalik <paul.michalik@outlook.com>
2018-09-20 09:51:57 +0200

    (1) Add test case Returns_Closest_Two_For_Four_People (2) Make fixture initialization more convenient

--- test/UnitTests.cpp
+++ test/UnitTests.cpp
@@ -10,19 +10,14 @@ using std::vector;
 class TestFixture : public testing::Test
 {
   protected:
-    Person sue;
-    Person greg;
-    Person sarah;
-    Person mike;
+    Person 
+        sue{"Sue", {1950, 0, 1}},
+        greg{"Greg", {1952, 5, 1}},
+        sarah{"Sarah", {1982, 0, 1}},
+        mike{"Mike", {1979, 0, 1}};
 
   public:
-    TestFixture()
-    {
-        sue = Person{"Sue", {1950, 0, 1}};
-        greg = Person{"Greg", {1952, 5, 1}};
-        sarah = Person{"Sarah", {1982, 0, 1}};
-        mike = Person{"Mike", {1979, 0, 1}};
-    }
+    TestFixture() = default;
 };
 
 TEST_F(TestFixture, Returns_Empty_Results_When_Given_Empty_List)
@@ -84,4 +79,18 @@ TEST_F(TestFixture, Returns_Furthest_Two_For_Four_People)
     Pair result = finder.Find(DifferenceType::Maximal);
     ASSERT_EQ(sue, result.Younger());
     ASSERT_EQ(sarah, result.Older());
+}
+
+TEST_F(TestFixture, Returns_Closest_Two_For_Four_People)
+{
+    vector<Person> list;
+    list.push_back(sue);
+    list.push_back(sarah);
+    list.push_back(mike);
+    list.push_back(greg);
+    Finder finder(list);
+
+    Pair result = finder.Find(DifferenceType::Minimal);
+    ASSERT_EQ(sue, result.Younger());
+    ASSERT_EQ(greg, result.Older());
 }
