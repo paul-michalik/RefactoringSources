678c1ec0cb367edd6e1498a6a244c66b07314192
Paul Michalik <paul.michalik@outlook.com>
2018-09-20 09:54:09 +0200

    (1) "Finder" uses more efficient and idiomatic implementation  (2) Make internal member functions protected to support testability

--- src/Finder.h
+++ src/Finder.h
@@ -2,7 +2,9 @@
 #define FILTER_FINDER_H
 
 #include <vector>
-
+#include <algorithm>
+#include <limits>
+#include <cassert>
 #include "Pair.h"
 #include "Person.h"
 
@@ -10,55 +12,53 @@ namespace algorithm {
 
 enum class DifferenceType
 {
-    Minimal,
-    Maximal
+    Minimal = 0,
+    Maximal = 1
 };
 
 class Finder {
 private:
   std::vector<Person> _p;
 
+protected:
+  static auto findMinDifference(std::vector<Person> const& p_)
+  {
+      assert(2 <= p_.size());
+      Pair res;
+      auto curMin = std::numeric_limits<long>::max(); 
+      auto b = p_.begin(), n = b, e = p_.end();
+      n++;
+      for (; n != e; ++b, ++n) {
+          auto locMin = n->getBirthDate().getTime() - b->getBirthDate().getTime();
+          if (locMin < curMin) {
+              curMin = locMin;
+              res = Pair{*b, *n};
+          }
+      }
+      return res;
+  }
+
+  static auto findMaxDifference(std::vector<Person> const& p_)
+  {
+      assert(2 <= p_.size());
+      return Pair{p_.front(), p_.back()};
+  }
+
+  auto getFindFunction(DifferenceType diffType) const
+  {
+      return diffType == DifferenceType::Minimal ? findMinDifference : findMaxDifference; 
+  }
+
 public:
   Finder(std::vector<Person> p) : _p{std::move(p)} 
   {
+      std::sort(_p.begin(), _p.end(), [](auto const& l_, auto const& r_) { return l_.getBirthDate() < r_.getBirthDate();
+      });
   }
 
   Pair Find(DifferenceType diffType)
   {
-    std::vector<Pair> tr;
-
-    if (_p.size() == 0)
-      return Pair{};
-
-    for (int i = 0; i < _p.size() - 1; i++) {
-      for (int j = i + 1; j < _p.size(); j++) {
-        Pair r{_p[i], _p[j]};
-        tr.push_back(r);
-      }
-    }
-
-    if (tr.size() < 1) {
-      return Pair{};
-    }
-
-    auto answer = tr[0];
-    for (auto const& result : tr) {
-      switch (diffType) {
-      case DifferenceType::Minimal:
-        if (result.AgeDifference() < answer.AgeDifference()) {
-          answer = result;
-        }
-        break;
-
-      case DifferenceType::Maximal:
-        if (result.AgeDifference() > answer.AgeDifference()) {
-          answer = result;
-        }
-        break;
-      }
-    }
-
-    return answer;
+      return _p.size() < 2 ? Pair{} : getFindFunction(diffType)(_p);
   }
 };
 }
